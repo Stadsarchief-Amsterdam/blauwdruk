a62a0f11376cbb1f1d257f0ce29f81592c8641a6
ivozandhuis <ivo@zandhuis.nl>
2021-09-30 15:53:40 +0200

    Added experiments with Memorix

---
+++ memorix-experiments/shacl4memorix.md
@@ -0,0 +1,19 @@
+# shacl4memorix.md
+
+## Dit mapje
+Dit mapje bevat alleen maar experimenten en schrijft op geen enkele manier voor hoe het gaat worden.
+
+## Geleerd over Memorix:
+(Deze tekst is nog lang niet uitgewerkt en vooral mijn opsomming van dingen die ik ervaren heb bij het experimenteren)
+
+* dc:identifier is verplicht en de waarde wordt gebruikt voor de unieke naam in het systeem. Het resulteert uiteindelijk in de naam van de memorix:Recordtype. Het mag geen taaldeclaratie bevatten want dan krijg je een foutmelding.
+
+* sh:group is verplicht
+
+* @prefix voor de sh:NodeShapes/memorix:Recordtype's MOET /resources/recordtypes/ (relatief pad?) zijn.
+
+* Als je een class wilt gebruiken om een knoop in het pad te willen definieren, moet je sh:BlankNode gebruiken
+
+* Bij het maken van details moet de class mee opgeslagen worden dat gebeurt door de sh:targetClass. 
+
+* Er mag maar e1n memorix:Recordtype in de request zitten. 
--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ export PATH=$PATH:$JENA_HOME/bin
 
 shacl --version
 
-shacl validate -s saa-sh.ttl -d 01-archief/correct/archief-correct1.ttl
-shacl validate -s saa-sh.ttl -d 01-archief/incorrect/archief-incorrect1.ttl
+# shacl validate -s saa-sh.ttl -d 01-archief/correct/archief-correct1.ttl
+# shacl validate -s saa-sh.ttl -d 01-archief/incorrect/archief-incorrect1.ttl
 # shacl validate -s saa-sh.ttl -d 04-afbeelding/correct/afbeelding-correct2.ttl
 # shacl validate -s saa-sh.ttl -d 05-informatieobject/correct/informatieobject-correct1.ttl
 # shacl validate -s saa-sh.ttl -d 05-informatieobject/correct/informatieobject-minimal1.ttl
@@ -16,4 +16,7 @@ shacl validate -s saa-sh.ttl -d 01-archief/incorrect/archief-incorrect1.ttl
 # shacl validate -s saa-sh.ttl -d ead-examples/394/394.rdf.xml
 # shacl validate -s saa-sh.ttl -d ead-examples/394/correct-fragment-394.ttl
 
+# shacl validate -s person-example/Persoonsvermelding.ttl -d person-example/person.in.ttl
+# shacl validate -s person-example/Persoonsvermelding.ttl -d person-example/person.out.ttl
+shacl validate -s blauwdruk.ttl -d 5e16843a-fe24-4fe9-9207-5cb57e586584.ttl
 
