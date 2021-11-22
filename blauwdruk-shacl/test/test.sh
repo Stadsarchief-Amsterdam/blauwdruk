568a3ebfdc4e69eae44517cdc3d4c4804b4b354f
ivozandhuis <ivo@zandhuis.nl>
2021-11-22 15:44:00 +0100

    Added blauwdruk/akte.ttl and tests

--- blauwdruk-shacl/test/test.sh
+++ blauwdruk-shacl/test/test.sh
@@ -5,12 +5,28 @@ export PATH=$PATH:$JENA_HOME/bin
 
 shacl --version
 
-# shacl validate -s ../archiefblok.ttl -d archiefblok/archiefblok-correct1.ttl
-# shacl validate -s ../archiefblok.ttl -d archiefblok/archiefblok-incorrect1.ttl
+echo ARCHIEFBLOK
+echo == Validate archiefblok-correct1.ttl:
+shacl validate -s ../archiefblok.ttl -d archiefblok/archiefblok-correct1.ttl
+echo == Validate archiefblok-incorrect1.ttl:
+shacl validate -s ../archiefblok.ttl -d archiefblok/archiefblok-incorrect1.ttl
 
-# shacl validate -s ../groep.ttl -d groep/groep-correct1.ttl
-# shacl validate -s ../groep.ttl -d groep/groep-incorrect1.ttl
+echo GROEP
+echo == Validate groep-correct1.ttl:
+shacl validate -s ../groep.ttl -d groep/groep-correct1.ttl
+echo == Validate groep-incorrect1.ttl
+shacl validate -s ../groep.ttl -d groep/groep-incorrect1.ttl
 
+echo BESTANDDEEL
+echo == Validate bestanddeel-correct1.ttl
 shacl validate -s ../bestanddeel.ttl -d bestanddeel/bestanddeel-correct1.ttl
+echo == Validate bestanddeel-incorrect1.ttl
 shacl validate -s ../bestanddeel.ttl -d bestanddeel/bestanddeel-incorrect1.ttl
 
+echo AKTE
+echo == Validate akte-correct1.ttl
+shacl validate -s ../akte.ttl -d akte/akte-correct1.ttl
+echo == Validate akte-incorrect1.ttl
+shacl validate -s ../akte.ttl -d akte/akte-incorrect1.ttl
+echo == Validate akte-minimal1.ttl
+shacl validate -s ../akte.ttl -d akte/akte-minimal1.ttl
