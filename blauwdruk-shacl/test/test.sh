2e3e1016d3f6e4f425eefc317ba28b83f2b37900
ivozandhuis <ivo@zandhuis.nl>
2021-11-22 10:09:53 +0100

    Tested and corrected 02-groep

--- blauwdruk-shacl/test/test.sh
+++ blauwdruk-shacl/test/test.sh
@@ -5,6 +5,10 @@ export PATH=$PATH:$JENA_HOME/bin
 
 shacl --version
 
-shacl validate -s ../01-archiefblok.ttl -d 01-archiefblok/archiefblok-correct1.ttl
-shacl validate -s ../01-archiefblok.ttl -d 01-archiefblok/archiefblok-incorrect1.ttl
+# shacl validate -s ../01-archiefblok.ttl -d 01-archiefblok/archiefblok-correct1.ttl
+# shacl validate -s ../01-archiefblok.ttl -d 01-archiefblok/archiefblok-incorrect1.ttl
+
+shacl validate -s ../02-groep.ttl -d 02-groep/groep-correct1.ttl
+# shacl validate -s ../02-groep.ttl -d 02-groep/groep-incorrect1.ttl
+
 
