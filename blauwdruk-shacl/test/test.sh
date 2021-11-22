6734d25cd7cfd77ed9aa1e408cfe63153dcf0091
ivozandhuis <ivo@zandhuis.nl>
2021-11-22 09:48:47 +0100

    Added 02-groep.ttl and first testing

---
+++ blauwdruk-shacl/test/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export JENA_HOME=/usr/bin/apache-jena/apache-jena-3.14.0
+export PATH=$PATH:$JENA_HOME/bin
+
+shacl --version
+
+shacl validate -s ../01-archiefblok.ttl -d 01-archiefblok/archiefblok-correct1.ttl
+shacl validate -s ../01-archiefblok.ttl -d 01-archiefblok/archiefblok-incorrect1.ttl
+
