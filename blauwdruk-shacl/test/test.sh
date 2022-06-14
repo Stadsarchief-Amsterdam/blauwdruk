e2affdd8af7d08410968c55ae22a995c037fa05d
ivozandhuis <ivo@zandhuis.nl>
2022-06-14 12:36:49 +0200

    Creating blauwdruk

--- blauwdruk-shacl/test/test.sh
+++ blauwdruk-shacl/test/test.sh
@@ -7,26 +7,26 @@ shacl --version
 
 echo ARCHIEFBLOK
 echo == Validate archiefblok-correct1.ttl:
-shacl validate -s ../archiefblok.ttl -d archiefblok/archiefblok-correct1.ttl
+shacl validate -s ../recordset.ttl -d archiefblok/archiefblok-correct1.ttl
 echo == Validate archiefblok-incorrect1.ttl:
-shacl validate -s ../archiefblok.ttl -d archiefblok/archiefblok-incorrect1.ttl
+shacl validate -s ../recordset.ttl -d archiefblok/archiefblok-incorrect1.ttl
 
 echo GROEP
 echo == Validate groep-correct1.ttl:
-shacl validate -s ../groep.ttl -d groep/groep-correct1.ttl
+shacl validate -s ../recordset.ttl -d groep/groep-correct1.ttl
 echo == Validate groep-incorrect1.ttl
-shacl validate -s ../groep.ttl -d groep/groep-incorrect1.ttl
+shacl validate -s ../recordset.ttl -d groep/groep-incorrect1.ttl
 
 echo BESTANDDEEL
 echo == Validate bestanddeel-correct1.ttl
-shacl validate -s ../bestanddeel.ttl -d bestanddeel/bestanddeel-correct1.ttl
+shacl validate -s ../recordset.ttl -d bestanddeel/bestanddeel-correct1.ttl
 echo == Validate bestanddeel-incorrect1.ttl
-shacl validate -s ../bestanddeel.ttl -d bestanddeel/bestanddeel-incorrect1.ttl
+shacl validate -s ../recordset.ttl -d bestanddeel/bestanddeel-incorrect1.ttl
 
 echo AKTE
 echo == Validate akte-correct1.ttl
-shacl validate -s ../akte.ttl -d akte/akte-correct1.ttl
+shacl validate -s ../record.ttl -d akte/akte-correct1.ttl
 echo == Validate akte-incorrect1.ttl
-shacl validate -s ../akte.ttl -d akte/akte-incorrect1.ttl
+shacl validate -s ../record.ttl -d akte/akte-incorrect1.ttl
 echo == Validate akte-minimal1.ttl
-shacl validate -s ../akte.ttl -d akte/akte-minimal1.ttl
+shacl validate -s ../record.ttl -d akte/akte-minimal1.ttl
